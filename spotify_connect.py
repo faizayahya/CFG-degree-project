42490e79836fe8d8baef88e6d563dcafd731873e
Aaliya Manji <anm_128@yahoo.co.uk>
2022-10-12 21:51:46 -0700

    experimenting with spotify feature extraction

--- spotify_connect.py
+++ spotify_connect.py
@@ -11,7 +11,20 @@ scope = "user-library-read user-top-read user-follow-read playlist-modify-public
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+res = sp.featured_playlists(locale='en_GB', country='GB', timestamp=None, limit=20, offset=0)
+playlists = res['playlists']['items']
+playlist = playlists[0]
+playlist_id = playlist['id']
+
+tracks = sp.playlist_items(playlist_id, fields=None, limit=10, offset=0, market='GB',
+                           additional_types=('track', 'episode'))
+
+for track in tracks['items']:
+    song = track['track']
+    song_id = song['id']
+
+    features = sp.audio_features(tracks=[song_id])
+    print(song['name'])
+    print('danceability: {}'.format(features[0]['danceability']))
+    print('valence: {}'.format(features[0]['valence']))
+    print('energy: {}'.format(features[0]['energy']))
