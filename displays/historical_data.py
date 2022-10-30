0876fcf788bdc93a414bc0047a0019ef0f9f27d5
Humaira Hossain <humairahossain@hotmail.com>
2022-10-30 13:40:08 +0000

    function to display links in a table

--- displays/historical_data.py
+++ displays/historical_data.py
@@ -1,12 +1,15 @@
-# this file contains functions to view all historical data
-
-# each user should be able to see a table containing date, mood score, playlist link
-
-# from db_utils USE def fetch_playlist_mood_data(username)
-# which gets copy of user's whole playlist history/mood history
-# then you need to create another method to loop through it to display it
-# or try and pretty print it
-# or print it in a table in terminal
+from db_files.db_utils import *
+from prettytable import PrettyTable
 
 
-# each user should ge able to generate a mood_score over time graph
+def create_table(history):
+    table = PrettyTable(['DATE', 'MOOD SCORE', 'LINK'])
+    for row in history:
+        table.add_row(row)
+
+    return table
+
+#
+user_history = (InPlaylistsTable.fetch_playlist_mood_data("humi"))  # --> list of tuples
+#
+print(create_table(user_history))
